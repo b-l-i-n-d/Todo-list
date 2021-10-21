c8f14eab9e9b9ef3ba70d47fe637921399a7e612
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-10-21 10:20:12 +0600

    tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': "'Poppins', sans-serif",
+        'ubuntu': "'Ubuntu', sans-serif"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
