8e847aeaa5d0f972b6df02600b07ec3d1b276da1
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-10-21 20:45:43 +0600

    basic functionality added

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,19 +1,36 @@
 const inputText = document.getElementById('inputText');
+const inputDesc = document.getElementById('inputDesc');
 const submitBtn = document.getElementById('submitBtn');
+const sortInprogressBtn = document.getElementById('sortInprogress');
 const list = document.getElementById('list');
-const listItems = list.getElementsByTagName('li');
-console.log("🚀 ~ file: script.js ~ line 5 ~ listItems", listItems)
+const listItems = list.childNodes;
 
 submitBtn.addEventListener('click', () => {
-    if (inputText.value) {
-        console.log("🚀 ~ file: script.js ~ line 6 ~ submitBtn.addEventListener ~ inputText.innerHTML", inputText.value);
-        addToList(inputText.value);
+    if (inputText.value || inputDesc.value) {
+        addToList(inputText.value, inputDesc.value);
     }
-})
+});
 
-addToList = (text) => {
+sortInprogressBtn.addEventListener('click', () => {
+    sortInprogressEvent();
+});
+
+addToList = (text, desc) => {
     let node = document.createElement('li');
-    let textNode = document.createTextNode(text);
+
+    let textNode = document.createElement('h3');
+    textNode.className = 'inline';
+    textNode.contentEditable = true;
+    textNode.innerHTML = text;
+
+    let textNodeDesc = document.createElement('p');
+    textNodeDesc.className = 'inline';
+    textNodeDesc.contentEditable = true;
+    textNodeDesc.innerHTML = desc;
+
+    let inprogressBtn = document.createElement('button');
+    inprogressBtn.className = 'inprogressBtn';
+    inprogressBtn.innerHTML = 'In progress';
 
     let checkBtn = document.createElement('button');
     checkBtn.className = 'checkBtn';
@@ -23,21 +40,74 @@ addToList = (text) => {
     deleteBtn.className = 'deleteBtn';
     deleteBtn.innerHTML = 'Delete';
 
-    node.append(checkBtn, textNode, deleteBtn)
+    node.append(textNode, textNodeDesc, checkBtn, inprogressBtn, deleteBtn);
     list.appendChild(node);
 
-    checkEvent(checkBtn, node);
+    checkEvent(checkBtn, node, textNode, textNodeDesc);
+    inprogressEvent(inprogressBtn, textNode, textNodeDesc, node);
     deleteEvent(deleteBtn, node);
 }
 
-checkEvent = (object, object2) => {  
-    object.addEventListener('click', () => {
-        object2.classList.toggle('line-through');
+checkEvent = (checkBtn, node, textNode, textNodeDesc) => {  
+    checkBtn.addEventListener('click', () => {
+        node.classList.toggle('line-through');
+        node.classList.toggle('checked');
+        textNode.classList.remove('text-3xl');
+        textNodeDesc.classList.remove('text-3xl');
+
+        sortCheckedEvent();
     })
 }
 
-deleteEvent = (object, object2) => {
-    object.addEventListener('click', () => {
-        object2.remove();
+deleteEvent = (deleteBtn, node) => {
+    deleteBtn.addEventListener('click', () => {
+        node.remove();
     })
+}
+
+inprogressEvent = (inprogressBtn, textNode, textNodeDesc, node) => {
+    inprogressBtn.addEventListener('click', () => {
+        textNode.classList.toggle('text-3xl');
+        textNodeDesc.classList.toggle('text-3xl');
+        node.classList.toggle('inprogress');
+        node.classList.remove('checked','line-through');
+    })
+}
+
+sortCheckedEvent = () => {
+    let listItemsArr = [];
+
+    for (let i in listItems) {
+        if (listItems[i].nodeType == 1) {
+            listItemsArr.push(listItems[i]);
+        }
+    }
+
+    listItemsArr.sort((a, b) => {
+        return a.classList.contains('checked') > b.classList.contains('checked') ? 1 : -1;
+    });
+
+    for (let i = listItemsArr.length - 1 ; i >= 0; i--) {
+        list.appendChild(listItemsArr[i])
+    }
+
+}
+
+sortInprogressEvent = () => {
+    let listItemsArr = [];
+
+    for (let i in listItems) {
+        if (listItems[i].nodeType == 1) {
+            listItemsArr.push(listItems[i]);
+        }
+    }
+
+    listItemsArr.sort((a, b) => {
+        return a.classList.contains('inprogress') > b.classList.contains('inprogress') ? 1 : -1;
+    });
+
+    for (let i = listItemsArr.length - 1 ; i >= 0; i--) {
+        list.appendChild(listItemsArr[i])
+    }
+
 }
