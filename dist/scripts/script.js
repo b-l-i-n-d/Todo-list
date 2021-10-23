58e049c489fd1dc8e3750b34beeec38722b9a88a
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-10-23 15:30:51 +0600

    Added navar

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -100,25 +100,6 @@ sortCheckedEvent = () => {
 
 }
 
-// sortInprogressEvent = () => {
-//     let listItemsArr = [];
-
-//     for (let i in listItems) {
-//         if (listItems[i].nodeType == 1) {
-//             listItemsArr.push(listItems[i]);
-//         }
-//     }
-
-//     listItemsArr.sort((a, b) => {
-//         return a.classList.contains('inprogress') > b.classList.contains('inprogress') ? 1 : -1;
-//     });
-
-//     for (let i = listItemsArr.length - 1 ; i >= 0; i--) {
-//         list.appendChild(listItemsArr[i])
-//     }
-
-// }
-
 filterList = (category) => {
 
     if (category == 'all') {
@@ -159,3 +140,42 @@ removeClass = (element, clName) => {
 
     element.className = classArray.join(" ");
 }
+
+var openmodal = document.querySelectorAll('.modal-open')
+    for (var i = 0; i < openmodal.length; i++) {
+      openmodal[i].addEventListener('click', function(event){
+    	event.preventDefault()
+    	toggleModal()
+      })
+    }
+    
+    const overlay = document.querySelector('.modal-overlay')
+    overlay.addEventListener('click', toggleModal)
+    
+    var closemodal = document.querySelectorAll('.modal-close')
+    for (var i = 0; i < closemodal.length; i++) {
+      closemodal[i].addEventListener('click', toggleModal)
+    }
+    
+    document.onkeydown = function(evt) {
+      evt = evt || window.event
+      var isEscape = false
+      if ("key" in evt) {
+    	isEscape = (evt.key === "Escape" || evt.key === "Esc")
+      } else {
+    	isEscape = (evt.keyCode === 27)
+      }
+      if (isEscape && document.body.classList.contains('modal-active')) {
+    	toggleModal()
+      }
+    };
+    
+    
+    function toggleModal () {
+      const body = document.querySelector('body')
+      const modal = document.querySelector('.modal')
+      modal.classList.toggle('opacity-0')
+      modal.classList.toggle('pointer-events-none')
+      body.classList.toggle('modal-active')
+    }
+    
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       fontFamily: {
         'poppins': "'Poppins', sans-serif",
         'ubuntu': "'Ubuntu', sans-serif"
+      },
+
+      colors: {
+        '69665c': '#69665c',
+        'b2afa1': '#b2afa1',
+        'fff9de': '#fff9de',
+        'd1e5f7': '#d1e5f7',
+        'daf2a6': '#daf2a6',
+        'ffcece': '#ffcece',
+        'd2ceff': '#d2ceff'
       }
     },
   },
