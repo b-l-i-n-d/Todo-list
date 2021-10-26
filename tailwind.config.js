3072f555c60c683c2078ee6aff39a14170198602
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-10-26 14:06:26 +0600

    Tailwind css purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    purge: [],
+    purge: {
+        enabled: false,
+        content: ['./dist/index.html']
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
